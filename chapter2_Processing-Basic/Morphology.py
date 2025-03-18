5defaecc9369f6863ff4866632a8f4b61a0bb203
Gilkyeong <2243869@donga.ac.kr>
2025-03-18 17:14:29 +0900

    Update Morphology.py

--- chapter2_Processing-Basic/Morphology.py
+++ chapter2_Processing-Basic/Morphology.py
@@ -2,17 +2,30 @@ import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
 
-image = cv.imread('JohnHancocksSignature.png', cv.IMREAD_GRAYSCALE)  
+image = cv.imread('JohnHancocksSignature.png', cv.IMREAD_UNCHANGED)
 
-kernel = cv.getStructuringElement(cv.MORPH_RECT, (5, 5))
+# Otsu
+_, b_image = cv.threshold(image[:, :, 3], 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
 
-Dilation = cv.morphologyEx(image, cv.MORPH_DILATE, kernel)
-Erosion = cv.morphologyEx(image, cv.MORPH_ERODE, kernel)
-Open = cv.morphologyEx(image, cv.MORPH_OPEN, kernel)
-Close = cv.morphologyEx(image, cv.MORPH_CLOSE, kernel)
+binary = b_image[b_image.shape[0] // 2:b_image.shape[0], 0:b_image.shape[0] // 2 + 1]
 
-result = np.hstack((image, Dilation, Erosion, Open, Close))
-cv.imshow('Binary | Dilation | Erosion | Opening | Closing', result)
-cv.imwrite('morphology_result.png', result)
+se = np.uint8([[0, 0, 1, 0, 0],
+               [0, 1, 1, 1, 0],
+               [1, 1, 1, 1, 1],
+               [0, 1, 1, 1, 0],
+               [0, 0, 1, 0, 0]])
+
+# 팽창
+Dilation = cv.dilate(binary, se, iterations=1)
+# 침식
+Erosion = cv.erode(binary, se, iterations=1)
+# 닫기
+Close = cv.morphologyEx(binary, cv.MORPH_CLOSE, se)
+# 열기
+Open = cv.morphologyEx(binary, cv.MORPH_OPEN, se)
+
+result = np.hstack((binary, Dilation, Erosion, Close, Open))
+
+cv.imshow('result', result)
 cv.waitKey(0)
 cv.destroyAllWindows()
